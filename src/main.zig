7428d5716d4a65062feb3ab99bca7f7fb8ad9d8f
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2024-12-23 21:53:21 +0000

    main self tests and uses game module

--- src/main.zig
+++ src/main.zig
@@ -1,43 +1,43 @@
 const std = @import("std");
-const curses = @cImport(@cInclude("curses.h"));
-const level = @import("level.zig");
+const curses = @import("curses.zig");
+const game = @import("game.zig");
 
 // TODO terminal size, map size constants
 
 pub fn main() !void {
-    const allocator = std.heap.c_allocator; // TODO better options?
-    const map = try level.Map.init(allocator, 24, 80);
-    defer map.deinit();
-    try map.drawRoom(.{ 5, 5 }, .{ 15, 15 });
-
-    _ = curses.initscr();
-
-    for (0..24) |y| {
-        for (0..80) |x| {
-            const c = try map.charAt(.{ @truncate(x), @truncate(y) });
-            _ = curses.mvaddch(@intCast(y), @intCast(x), c);
-        }
-    }
-    _ = curses.refresh();
-    _ = curses.getch();
-    _ = curses.endwin();
-}
-
-test "run the game" {
-    const game = @import("game.zig");
-    const MockDisplayProvider = @import("display.zig").MockDisplayProvider;
-    const MockInputProvider = @import("input.zig").MockInputProvider;
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
+    var providers = try curses.init(allocator);
+    const display = providers.display.provider();
+    const input = providers.input.provider();
+    defer display.endwin();
+
+    try game.run(allocator, input, display);
+}
+
+//
+// Unit tests
+//
+
+test "run the game" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    // TODO: use testing allocator
+
+    const MockDisplayProvider = @import("display.zig").MockDisplayProvider;
+    const MockInputProvider = @import("input.zig").MockInputProvider;
+
     var md = MockDisplayProvider.init(.{ .maxx = 80, .maxy = 24 });
     const display = md.provider();
+    defer display.endwin();
 
     var mi = MockInputProvider.init(.{ .keypress = '*' });
     const input = mi.provider();
 
     try game.run(allocator, input, display);
+
+    // TODO: must be a way to test for all leaks (of display still init, etc.)
 }
 
 // EOF
