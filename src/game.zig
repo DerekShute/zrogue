4d5a0bbbf4d35b7fbd4ec34f9f23d39cfcaaa497
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2024-12-31 22:17:51 +0000

    player Thing

--- src/game.zig
+++ src/game.zig
@@ -3,19 +3,24 @@ const DisplayProvider = @import("display.zig").DisplayProvider;
 const InputProvider = @import("input.zig").InputProvider;
 
 const level = @import("level.zig");
+const Thing = @import("thing.zig").Thing;
 
 // TODO: test maxx, maxy
 pub fn run(allocator: std.mem.Allocator, input: InputProvider, display: DisplayProvider) !void {
     const map = try level.Map.init(allocator, 24, 80);
+    var player = Thing{ .xy = .{ 10, 10 }, .ch = '@' };
+
     defer map.deinit();
 
+    try map.setMonster(&player, .{ 10, 10 });
     try display.erase();
 
     try map.drawRoom(.{ 5, 5 }, .{ 15, 15 });
 
+    // TODO: Some kind of display abstraction or put in the display provider
     for (0..24) |y| {
         for (0..80) |x| {
-            const c = try map.charAt(.{ @truncate(x), @truncate(y) });
+            const c = try map.getChar(.{ @truncate(x), @truncate(y) });
             try display.mvaddch(@intCast(x), @intCast(y), c);
         }
     }
--- src/level.zig
+++ src/level.zig
@@ -1,19 +1,37 @@
 const std = @import("std");
-
-// ===================
-// Structure for monsters and objects
-
-const Thing = struct {
-    x: u8,
-};
+const Thing = @import("thing.zig").Thing;
 
 // ===================
 // Spot on the map
 
 const Place = struct {
-    ch: u8,
-    flags: u8, // TODO room flags as packed sruct(u8)
-    monst: Thing, // TODO is a list, can be empty
+    ch: u8 = ' ', // TODO manifest constant
+    flags: u8 = 0, // TODO room flags as packed struct(u8)
+    monst: ?*Thing = null,
+
+    pub fn getChar(self: *Place) u8 {
+        if (self.monst) |monst| {
+            return monst.getChar();
+        }
+        return self.ch;
+    }
+
+    pub fn setChar(self: *Place, tochar: u8) void {
+        self.ch = tochar;
+    }
+
+    pub fn getMonst(self: *Place) ?*Thing {
+        return self.monst;
+    }
+
+    pub fn setMonst(self: *Place, new_monst: *Thing) !void {
+        if (self.monst) |_| {
+            return error.AlreadyOccupied; // TODO collect errors
+        }
+        self.monst = new_monst;
+    }
+
+    // TODO: getFlags(), setFlags()
 };
 
 pub const Map = struct {
@@ -37,9 +55,9 @@ pub const Map = struct {
         // TODO this blows up u8 height width
         map.places = try allocator.alloc(Place, height * width);
         for (map.places) |*place| {
-            place.ch = ' '; // TODO constants
+            place.setChar(' '); // TODO constants
             place.flags = 0;
-            place.monst.x = 0;
+            place.monst = null;
         }
 
         return map;
@@ -55,24 +73,34 @@ pub const Map = struct {
         allocator.destroy(self);
     }
 
-    pub fn charAt(self: *Map, xy: [2]u16) !u8 {
+    // Utility
+
+    fn toPlace(self: *Map, xy: [2]u16) !*Place {
         if (xy[0] >= self.width)
             return error.OverFlow;
         if (xy[1] >= self.height)
             return error.OverFlow;
 
         const loc = xy[0] + xy[1] * self.width;
-        return self.places[loc].ch;
+        return &self.places[loc];
     }
 
-    pub fn monsterAt(self: *Map, xy: [2]u16) !?*Thing {
-        if (xy[0] >= self.width)
-            return error.OverFlow;
-        if (xy[1] >= self.height)
-            return error.OverFlow;
+    // Methods
 
-        const loc = xy[0] + xy[1] * self.width;
-        return &self.places[loc].monst; // TODO could be nothing there
+    pub fn getChar(self: *Map, xy: [2]u16) !u8 {
+        const place = try self.toPlace(xy);
+        return place.getChar();
+    }
+
+    pub fn getMonster(self: *Map, xy: [2]u16) !?*Thing {
+        const place = try self.toPlace(xy);
+        return place.getMonst();
+    }
+
+    pub fn setMonster(self: *Map, monst: *Thing, xy: [2]u16) !void {
+        const place = try self.toPlace(xy);
+        try place.setMonst(monst);
+        monst.setPos(xy); // TODO is this the best way?
     }
 
     pub fn drawRoom(self: *Map, xy: [2]u16, max: [2]u16) !void {
@@ -81,14 +109,14 @@ pub const Map = struct {
             fn vert(places: []Place, width: u16, startx: u16, y: [2]u16) void {
                 for (y[0]..y[1] + 1) |at| {
                     const loc = startx + at * width;
-                    places[loc].ch = '|'; // TODO access function
+                    places[loc].setChar('|'); // TODO: manifest constant
                 }
             }
 
             fn horiz(places: []Place, width: u16, starty: u16, x: [2]u16) void {
                 for (x[0]..x[1] + 1) |at| {
                     const loc = at + starty * width;
-                    places[loc].ch = '-'; // TODO access function
+                    places[loc].setChar('-'); // TODO: manifest constant
                 }
             }
 
@@ -96,7 +124,7 @@ pub const Map = struct {
                 for (start[1]..limit[1] + 1) |c_y| {
                     for (start[0]..limit[0] + 1) |c_x| {
                         const loc = c_x + c_y * width;
-                        places[loc].ch = '.';
+                        places[loc].setChar('.'); // TODO: manifest constant
                     }
                 }
             }
@@ -128,7 +156,7 @@ pub const Map = struct {
 
 test "allocate and free" {
     const map: *Map = try Map.init(std.testing.allocator, 100, 100);
-    defer Map.deinit(map);
+    defer map.deinit();
 }
 
 test "fails to allocate any of map" { // first allocation attempt
@@ -145,67 +173,79 @@ test "allocate and free zero size" {
     // Not sure what the allocator does for 'allocate zero' but here you are
 
     const map: *Map = try Map.init(std.testing.allocator, 0, 0);
-    defer Map.deinit(map);
+    defer map.deinit();
 }
 
 test "ask about valid map location" {
     const map: *Map = try Map.init(std.testing.allocator, 10, 10);
-    defer Map.deinit(map);
+    defer map.deinit();
 
-    const thing = try Map.monsterAt(map, .{ 4, 4 });
-    try std.testing.expect(thing != null); // TODO only if something there
+    const thing = try map.getMonster(.{ 4, 4 });
+    try std.testing.expect(thing == null); // Nothing there
 }
 
 test "ask about thing at invalid map location" {
     const map: *Map = try Map.init(std.testing.allocator, 10, 10);
-    defer Map.deinit(map);
-    try std.testing.expectError(error.OverFlow, Map.monsterAt(map, .{ 0, 20 }));
-    try std.testing.expectError(error.OverFlow, Map.monsterAt(map, .{ 20, 0 }));
+    defer map.deinit();
+    try std.testing.expectError(error.OverFlow, map.getMonster(.{ 0, 20 }));
+    try std.testing.expectError(error.OverFlow, map.getMonster(.{ 20, 0 }));
 }
 
 test "ask about a character on the map" {
     const map: *Map = try Map.init(std.testing.allocator, 10, 10);
-    defer Map.deinit(map);
-    try std.testing.expect(try Map.charAt(map, .{ 0, 0 }) == ' ');
+    defer map.deinit();
+    try std.testing.expect(try map.getChar(.{ 0, 0 }) == ' ');
 }
 
 test "ask about invalid character on the map" {
     const map: *Map = try Map.init(std.testing.allocator, 10, 10);
-    defer Map.deinit(map);
-    try std.testing.expectError(error.OverFlow, Map.charAt(map, .{ 20, 0 }));
-    try std.testing.expectError(error.OverFlow, Map.charAt(map, .{ 0, 20 }));
+    defer map.deinit();
+    try std.testing.expectError(error.OverFlow, map.getChar(.{ 20, 0 }));
+    try std.testing.expectError(error.OverFlow, map.getChar(.{ 0, 20 }));
 }
 
 test "draw an invalid room" {
     const map: *Map = try Map.init(std.testing.allocator, 20, 20);
-    defer Map.deinit(map);
+    defer map.deinit();
 
-    try std.testing.expectError(error.OverFlow, Map.drawRoom(map, .{ 15, 15 }, .{ 4, 18 }));
-    try std.testing.expectError(error.OverFlow, Map.drawRoom(map, .{ 15, 15 }, .{ 18, 4 }));
+    try std.testing.expectError(error.OverFlow, map.drawRoom(.{ 15, 15 }, .{ 4, 18 }));
+    try std.testing.expectError(error.OverFlow, map.drawRoom(.{ 15, 15 }, .{ 18, 4 }));
 }
 
 test "draw an oversize room" {
     const map: *Map = try Map.init(std.testing.allocator, 20, 20);
-    defer Map.deinit(map);
+    defer map.deinit();
 
-    try std.testing.expectError(error.OverFlow, Map.drawRoom(map, .{ 0, 0 }, .{ 0, 100 }));
-    try std.testing.expectError(error.OverFlow, Map.drawRoom(map, .{ 0, 0 }, .{ 100, 0 }));
+    try std.testing.expectError(error.OverFlow, map.drawRoom(.{ 0, 0 }, .{ 0, 100 }));
+    try std.testing.expectError(error.OverFlow, map.drawRoom(.{ 0, 0 }, .{ 100, 0 }));
 }
 
 test "draw a valid room and test corners" {
     const map: *Map = try Map.init(std.testing.allocator, 50, 50);
-    defer Map.deinit(map);
+    defer map.deinit();
 
-    try Map.drawRoom(map, .{ 10, 10 }, .{ 20, 20 });
+    try map.drawRoom(.{ 10, 10 }, .{ 20, 20 });
     // Corners are '-'
-    try std.testing.expect(try Map.charAt(map, .{ 10, 10 }) == '-');
-    try std.testing.expect(try Map.charAt(map, .{ 20, 10 }) == '-');
-    try std.testing.expect(try Map.charAt(map, .{ 10, 20 }) == '-');
-    try std.testing.expect(try Map.charAt(map, .{ 20, 20 }) == '-');
-    try std.testing.expect(try Map.charAt(map, .{ 19, 19 }) == '.');
-    try std.testing.expect(try Map.charAt(map, .{ 11, 11 }) == '.');
-    try std.testing.expect(try Map.charAt(map, .{ 11, 19 }) == '.');
-    try std.testing.expect(try Map.charAt(map, .{ 19, 11 }) == '.');
+    try std.testing.expect(try map.getChar(.{ 10, 10 }) == '-');
+    try std.testing.expect(try map.getChar(.{ 20, 10 }) == '-');
+    try std.testing.expect(try map.getChar(.{ 10, 20 }) == '-');
+    try std.testing.expect(try map.getChar(.{ 20, 20 }) == '-');
+    try std.testing.expect(try map.getChar(.{ 19, 19 }) == '.');
+    try std.testing.expect(try map.getChar(.{ 11, 11 }) == '.');
+    try std.testing.expect(try map.getChar(.{ 11, 19 }) == '.');
+    try std.testing.expect(try map.getChar(.{ 19, 11 }) == '.');
+}
+
+test "putting monsters places" {
+    const map: *Map = try Map.init(std.testing.allocator, 50, 50);
+    defer Map.deinit(map);
+    var thing = Thing{ .xy = .{ 0, 0 }, .ch = '@' };
+    var thing2 = Thing{ .xy = .{ 0, 0 }, .ch = '@' };
+
+    try map.setMonster(&thing, .{ 10, 10 });
+    try std.testing.expect(thing.atPos(.{ 10, 10 }));
+
+    try std.testing.expectError(error.AlreadyOccupied, map.setMonster(&thing2, .{ 10, 10 }));
 }
 
 // EOF
