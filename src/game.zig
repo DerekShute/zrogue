20c89ae119ac792693dc05db4f04486d8bf5d5be
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2025-02-16 21:29:46 +0000

    BumpAction to MoveAction, etc

--- src/game.zig
+++ src/game.zig
@@ -31,9 +31,9 @@ pub fn run(allocator: std.mem.Allocator, player_thing: *Thing) !void {
         action = try player_thing.doAction(&map);
         switch (action.type) {
             ActionType.QuitAction => continue, // TODO: 'quitting' message
-            ActionType.BumpAction => try bumpAction(player_thing, &action, &map),
             ActionType.AscendAction => try ascendAction(player_thing, &action, &map),
             ActionType.DescendAction => try descendAction(player_thing, &action, &map),
+            ActionType.MoveAction => try moveAction(player_thing, &action, &map),
             ActionType.NoAction => continue,
         }
     }
@@ -49,12 +49,18 @@ fn ascendAction(entity: *Thing, do_action: *ThingAction, map: *Map) !void {
     entity.addMessage("No stairs here!");
 }
 
-fn bumpAction(entity: *Thing, do_action: *ThingAction, map: *Map) !void {
-    const pos = entity.getPos();
+fn descendAction(entity: *Thing, do_action: *ThingAction, map: *Map) !void {
+    _ = do_action;
+    _ = map;
+    entity.addMessage("No stairs here!");
+}
 
-    // TODO Law of Demeter here
-    const new_x = pos.getX() + do_action.pos.getX();
-    const new_y = pos.getY() + do_action.pos.getY();
+fn moveAction(entity: *Thing, do_action: *ThingAction, map: *Map) !void {
+    const pos = entity.getPos();
+    const newPos = do_action.getPos();
+
+    const new_x = pos.getX() + newPos.getX();
+    const new_y = pos.getY() + newPos.getY();
 
     if (try map.passable(new_x, new_y)) {
         try map.removeMonster(pos.getX(), pos.getY());
@@ -69,14 +75,9 @@ fn bumpAction(entity: *Thing, do_action: *ThingAction, map: *Map) !void {
             try map.setRegionKnown(new_x - 1, new_y - 1, new_x + 1, new_y + 1);
         }
     } else {
+        // TODO: entity 'bump' callback
         entity.addMessage("Ouch!");
     }
-}
-
-fn descendAction(entity: *Thing, do_action: *ThingAction, map: *Map) !void {
-    _ = do_action;
-    _ = map;
-    entity.addMessage("No stairs here!");
 }
 
 // EOF
