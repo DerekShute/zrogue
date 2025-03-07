398a9e3d368275b238805262b49c91a9df3180f0
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2025-03-07 19:58:06 +0000

    list manager as utility

--- unit_tests.zig
+++ unit_tests.zig
@@ -9,10 +9,11 @@ comptime {
     _ = @import("src/item.zig");
     _ = @import("src/map.zig");
     _ = @import("src/main.zig");
-    _ = @import("src/manager.zig");
     _ = @import("src/thing.zig");
     _ = @import("src/zrogue.zig");
     _ = @import("src/message_log.zig");
+
+    _ = @import("src/utils/tests.zig");
 }
 
 // EOF
