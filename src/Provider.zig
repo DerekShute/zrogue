cf55d56ea904344687acf3fd492dc968b73bd53d
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2025-05-20 15:30:47 +0000

    Refactor provider interfaces and errors

--- src/Provider.zig
+++ src/Provider.zig
@@ -29,20 +29,19 @@ pub const VTable = struct {
     // constructor/destructor
     endwin: *const fn (ctx: *anyopaque) void,
     // display
-    erase: *const fn (ctx: *anyopaque) Error!void,
-    getmaxx: *const fn (ctx: *anyopaque) Error!u16,
-    getmaxy: *const fn (ctx: *anyopaque) Error!u16,
-    mvaddch: *const fn (ctx: *anyopaque, x: u16, y: u16, ch: u8) Error!void,
     mvaddstr: *const fn (ctx: *anyopaque, x: u16, y: u16, s: []const u8) Error!void,
     refresh: *const fn (ctx: *anyopaque) Error!void,
     setTile: *const fn (ctx: *anyopaque, x: u16, y: u16, t: MapTile) Error!void,
     // input
-    getCommand: *const fn (ctx: *anyopaque) Error!Command,
+    getCommand: *const fn (ctx: *anyopaque) Command,
 };
 
 //
 // Structure Members
 //
+//  Hiding 'initialized' here would require back pointers from interface ctx
+//  to the Provider containment
+
 ptr: *anyopaque,
 vtable: *const VTable,
 
@@ -58,22 +57,6 @@ pub inline fn endwin(self: Self) void {
 // Methods
 //
 
-pub inline fn erase(self: Self) Error!void {
-    return self.vtable.erase(self.ptr);
-}
-
-pub inline fn getmaxx(self: Self) Error!u16 {
-    return self.vtable.getmaxx(self.ptr);
-}
-
-pub inline fn getmaxy(self: Self) Error!u16 {
-    return self.vtable.getmaxy(self.ptr);
-}
-
-pub inline fn mvaddch(self: Self, x: u16, y: u16, ch: u8) Error!void {
-    try self.vtable.mvaddch(self.ptr, x, y, ch);
-}
-
 pub inline fn mvaddstr(self: Self, x: u16, y: u16, s: []const u8) Error!void {
     try self.vtable.mvaddstr(self.ptr, x, y, s);
 }
@@ -86,7 +69,7 @@ pub inline fn setTile(self: Self, x: u16, y: u16, t: MapTile) Error!void {
     try self.vtable.setTile(self.ptr, x, y, t);
 }
 
-pub inline fn getCommand(self: Self) Error!Command {
+pub inline fn getCommand(self: Self) Command {
     return self.vtable.getCommand(self.ptr);
 }
 
@@ -127,10 +110,6 @@ pub const MockProvider = struct {
             .ptr = self,
             .vtable = &.{
                 .endwin = mock_endwin,
-                .erase = mock_erase,
-                .getmaxx = mock_getmaxx,
-                .getmaxy = mock_getmaxy,
-                .mvaddch = mock_mvaddch,
                 .mvaddstr = mock_mvaddstr,
                 .refresh = mock_refresh,
                 .setTile = mock_setTile,
@@ -149,64 +128,41 @@ pub const MockProvider = struct {
         return;
     }
 
-    fn mock_erase(ptr: *anyopaque) Error!void {
-        const self: *MockProvider = @ptrCast(@alignCast(ptr));
-        if (!self.initialized) {
-            return Error.NotInitialized;
-        }
-        return;
-    }
-
-    fn mock_getmaxx(ptr: *anyopaque) Error!u16 {
-        const self: *MockProvider = @ptrCast(@alignCast(ptr));
-        if (!self.initialized) {
-            return Error.NotInitialized;
-        }
-        return self.maxx;
-    }
-
-    fn mock_getmaxy(ptr: *anyopaque) Error!u16 {
-        const self: *MockProvider = @ptrCast(@alignCast(ptr));
-        if (!self.initialized) {
-            return Error.NotInitialized;
-        }
-        return self.maxy;
-    }
-
-    fn mock_mvaddch(ptr: *anyopaque, x: u16, y: u16, ch: u8) Error!void {
-        const self: *MockProvider = @ptrCast(@alignCast(ptr));
-        _ = x;
-        _ = y;
-        _ = self;
-        _ = ch;
-        return;
-    }
-
     fn mock_mvaddstr(ptr: *anyopaque, x: u16, y: u16, s: []const u8) Error!void {
         const self: *MockProvider = @ptrCast(@alignCast(ptr));
+        if (!self.initialized) {
+            return Error.NotInitialized;
+        }
         _ = x;
         _ = y;
-        _ = self;
         _ = s;
         return;
     }
 
     fn mock_refresh(ptr: *anyopaque) Error!void {
-        _ = ptr;
+        const self: *MockProvider = @ptrCast(@alignCast(ptr));
+        if (!self.initialized) {
+            return Error.NotInitialized;
+        }
         return;
     }
 
     fn mock_setTile(ptr: *anyopaque, x: u16, y: u16, t: MapTile) Error!void {
         const self: *MockProvider = @ptrCast(@alignCast(ptr));
+        if (!self.initialized) {
+            return Error.NotInitialized;
+        }
         _ = x;
         _ = y;
-        _ = self;
         _ = t;
         return;
     }
 
-    fn mock_getCommand(ptr: *anyopaque) !Command {
+    fn mock_getCommand(ptr: *anyopaque) Command {
         const self: *MockProvider = @ptrCast(@alignCast(ptr));
+        if (!self.initialized) {
+            @panic("mock_getCommand before initialized");
+        }
         const i = self.command_index;
         if (i >= self.command_list.len) {
             @panic("No more mock commands to provide");
@@ -220,6 +176,8 @@ pub const MockProvider = struct {
 // Unit tests
 //
 
+const expectError = std.testing.expectError;
+
 var testlist = [_]Command{
     .goWest,
     .quit,
@@ -230,11 +188,8 @@ test "Basic use of mock provider" {
     var d = p.provider();
     defer d.endwin();
 
-    try d.erase();
     try d.refresh();
-
-    try std.testing.expect(try d.getmaxx() == 40);
-    try std.testing.expect(try d.getmaxy() == 60);
+    try d.mvaddstr(0, 0, "frotz");
 }
 
 test "Method use after endwin" {
@@ -242,9 +197,10 @@ test "Method use after endwin" {
     var d = p.provider();
 
     d.endwin();
-    try std.testing.expectError(Error.NotInitialized, d.erase());
-    try std.testing.expectError(Error.NotInitialized, d.getmaxx());
-    try std.testing.expectError(Error.NotInitialized, d.getmaxy());
+    // getCommand will panic
+    try expectError(Error.NotInitialized, d.refresh());
+    try expectError(Error.NotInitialized, d.setTile(0, 0, .floor));
+    try expectError(Error.NotInitialized, d.mvaddstr(0, 0, "frotz"));
 }
 
 // Visualization
