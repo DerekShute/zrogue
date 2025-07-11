be970a4e2e98a0c5dc0a55fca69452c02d4fc09b
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2025-07-11 22:11:41 +0000

    split Region.zig

--- unit_tests.zig
+++ unit_tests.zig
@@ -9,6 +9,7 @@ comptime {
     _ = @import("src/main.zig");
     _ = @import("src/new_level.zig");
     _ = @import("src/Provider.zig");
+    _ = @import("src/Region.zig");
     _ = @import("src/thing.zig");
     _ = @import("src/zrogue.zig");
     _ = @import("src/message_log.zig");
