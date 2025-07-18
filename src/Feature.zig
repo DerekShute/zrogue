4b43573a2f2465c2110133e16e5d9f0b3031743c
Derek Shute <67981895+DerekShute@users.noreply.github.com>
2025-07-18 18:27:45 +0000

    Fill out Feature contents

--- src/Feature.zig
+++ src/Feature.zig
@@ -8,31 +8,78 @@ const Map = @import("map.zig").Map;
 
 const Self = @This();
 
+//
+// Vector Table
+//
+
 pub const VTable = struct {
     find: *const fn (self: *Self, map: *Map) bool,
     // Take, etc.
 };
 
-pub const Config = struct {
-    pos: zrogue.Pos,
-    vtable: ?*VTable,
-};
+//
+// Members
+//
 
 p: zrogue.Pos = undefined,
 vtable: ?*const VTable = null,
 
+//
+// Methods
+//
+
 pub fn find(self: *Self, map: *Map) bool {
+
+    // Entity searches the map location; this is the consequence of
+    // finding this feature.  Returns 'false' if not found, 'true' otherwise
+
     if (self.vtable) |v| {
         return v.find(self, map);
     }
     return false;
 }
 
+//
 // Import Pos functions
+//
 
 pub usingnamespace zrogue.Pos.Methods(@This());
 
+//
+// Unit test
+//
+
+const expect = std.testing.expect;
+
+fn testFind(self: *Self, map: *Map) bool {
+    _ = map;
+    _ = self;
+    return true;
+}
+
+const test_vtable: VTable = .{
+    .find = testFind,
+};
+
+test "Feature vtable execution" {
+    const m = try Map.init(std.testing.allocator, 50, 50, 1, 1);
+    defer m.deinit();
+    var f = Self{ .p = zrogue.Pos.init(1, 1), .vtable = &test_vtable };
+
+    try expect(find(&f, m) == true);
+}
+
+test "Feature vtable fallthrough" {
+    const m = try Map.init(std.testing.allocator, 50, 50, 1, 1);
+    defer m.deinit();
+    var f = Self{ .p = zrogue.Pos.init(1, 1), .vtable = null };
+
+    try expect(find(&f, m) == false);
+}
+
+//
 // Visualization
+//
 
 const genFields = @import("utils/visual.zig").genFields;
 pub var feature_fields = genFields(Self);
